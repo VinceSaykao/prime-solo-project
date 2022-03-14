585e4a663104c39e2156ee32e6f3cf1096d69503
vincesaykao <vtsaykao@hotmail.com>
2022-03-14 12:48:29 -0500

    added comments to router get

---
+++ server/routes/timesheet.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+
+// this will GET the timesheet from the database
+router.get('/', (req, res) => {
+    // GET route code here
+    if (req.isAuthenticated()) {
+        pool
+            .query(`select * from timesheet;`)
+            .then((results) => res.send(results.rows))
+            .catch((error) => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403); // Forbidden
+    };
+});
+
+
+        module.exports = router;
