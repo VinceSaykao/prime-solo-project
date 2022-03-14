0f9d1aa95a67eb3c59cf8d3eeede74321062cd43
vincesaykao <vtsaykao@hotmail.com>
2022-03-14 15:35:52 -0500

    set 6 inputs, 6 local states and given them values and onchange

--- src/components/TimesheetForm/TimesheetForm.js
+++ src/components/TimesheetForm/TimesheetForm.js
@@ -1,8 +1,52 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 export default function TimesheetForm() {
 
-    form (
+    const [clientName, setClientName] = useState('');
+    const [date, setDate] = useState('');
+    const [timeIn, setTimeIn] = useState('');
+    const [timeOut, setTimeOut] = useState('');
+    const [mileage, setMileage] = useState('');
+    const [notes, setNotes] = useState('');
+    return (
         <div>
-            
+
+            <p>Hello World</p>
+            <input
+                placeholder="Client Name"
+                value={clientName}
+                onChange={evt => setClientNAme(evt.target.value)}
+            />
+            <input
+                placeholder="Date"
+                value={date}
+                onChange={evt => setDate(evt.target.value)}
+            />
+            <input
+                placeholder="Time In"
+                value={timeIn}
+                onChange={evt => setTimeIn(evt.target.value)}
+            />
+            <input
+                placeholder="Time Out"
+                value={timeOut}
+                onChange={evt => setTimeOut(evt.target.value)}
+            />
+            <input
+                placeholder="Mileage"
+                value={mileage}
+                onChange={evt => setMileage(evt.target.value)}
+            />
+            <input
+                placeholder="Notes"
+                value={notes}
+                onChange={evt => setNotes(evt.target.value)}
+            />
+            <button>
+                Submit
+            </button>
+
         </div>
     )
 }; // end of TimesheetForm
