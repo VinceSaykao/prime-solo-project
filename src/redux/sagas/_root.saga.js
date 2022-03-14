0f53b586f53469a2a9d16c6a4b02fcbe4199118d
vincesaykao <vtsaykao@hotmail.com>
2022-03-14 16:19:41 -0500

    added console log in timesheetform.js and added watcher saga to addtimesheet saga and rootsaga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,7 +3,7 @@ import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import fetchTimesheet from './fetchTimesheet.saga';
-import addTimesheet from './addTimesheet.saga';
+import addWatcherSaga from './addTimesheet.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -18,6 +18,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     fetchTimesheet(),
-    addTimesheet(),
+    addWatcherSaga(),
   ]);
 }
