cc6299537bce745661e7853d6a207408c7a5f5b7
vincesaykao <vtsaykao@hotmail.com>
2022-03-14 21:07:12 -0500

    added edit button

--- src/components/TimesheetItem/TimesheetItem.js
+++ src/components/TimesheetItem/TimesheetItem.js
@@ -1,10 +1,17 @@
 
 export default function TimesheetItem({timesheetItem}) {
 
+    function handleEdit() {
+        console.log('clicked edit');
+    }; // end of handleEdit
+
     return (
         <div>
             <li>
             {timesheetItem.date} {timesheetItem.client_name} {timesheetItem.in} {timesheetItem.out} {timesheetItem.mileage} {timesheetItem.notes}
+            <button
+            onClick={handleEdit}
+            >Edit</button>
             </li>
         </div>
     )
