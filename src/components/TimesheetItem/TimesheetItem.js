773118ab1026399f26fe890c1213f4756ca73ca3
vincesaykao <vtsaykao@hotmail.com>
2022-03-14 21:21:19 -0500

    create updatetimesheet form and also added it into app.js

--- src/components/TimesheetItem/TimesheetItem.js
+++ src/components/TimesheetItem/TimesheetItem.js
@@ -1,8 +1,12 @@
+import { useHistory } from 'react-router-dom'
 
 export default function TimesheetItem({timesheetItem}) {
 
+    const history = useHistory();
+
     function handleEdit() {
         console.log('clicked edit');
+        history.push('/updateTimesheetForm')
     }; // end of handleEdit
 
     return (
---
+++ src/components/UpdateTimesheetForm/UpdateTimesheetForm.js
@@ -0,0 +1,10 @@
+
+
+export default function UpdateTimesheetForm() {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+
+}; // end of UpdateTimesheetForm
