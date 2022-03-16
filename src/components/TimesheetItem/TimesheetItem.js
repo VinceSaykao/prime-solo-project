810f38a5f5f8631e789ad10f66cfd6cb2864e4cc
vincesaykao <vtsaykao@hotmail.com>
2022-03-16 17:10:01 -0500

    reset back to workable dom

--- src/components/TimesheetItem/TimesheetItem.js
+++ src/components/TimesheetItem/TimesheetItem.js
@@ -2,7 +2,7 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import './TimesheetItem.scss';
 
-export default function TimesheetItem({timesheetItem}) {
+export default function TimesheetItem({ timesheetItem }) {
 
     const history = useHistory();
     const dispatch = useDispatch();
@@ -10,26 +10,28 @@ export default function TimesheetItem({timesheetItem}) {
 
     const handleEdit = () => {
         console.log('clicked edit');
-        dispatch ({type: 'SET_UPDATE_TIMESHEET', payload: timesheetItem})
+        dispatch({ type: 'SET_UPDATE_TIMESHEET', payload: timesheetItem })
         history.push('/updateTimesheetForm');
     }; // end of handleEdit
 
     const handleDelete = () => {
         console.log('clicked delete');
-        dispatch({type: 'DELETE_TIMESHEET', payload: timesheetItem.id})
+        dispatch({ type: 'DELETE_TIMESHEET', payload: timesheetItem.id })
     }; // end of handleDelete
 
     return (
         <div>
-            <li id='timesheet-item-list'>
-            {timesheetItem.date} {timesheetItem.client_name} {timesheetItem.in} {timesheetItem.out} {timesheetItem.mileage} {timesheetItem.notes}
-            <button
-            onClick={handleEdit}
-            >Edit</button>
-            <button
-            onClick={handleDelete}
-            >Delete</button>
-            </li>
+
+            <div id='timesheet-item-list'>
+                {timesheetItem.date} {timesheetItem.client_name} {timesheetItem.in} {timesheetItem.out} {timesheetItem.mileage} {timesheetItem.notes}
+                <button
+                    onClick={handleEdit}
+                >Edit</button>
+                <button
+                    onClick={handleDelete}
+                >Delete</button>
+            </div>
+
         </div>
     )
 }; // end of TimesheetItem
