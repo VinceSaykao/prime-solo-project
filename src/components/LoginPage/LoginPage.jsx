e4cfc1d287f95edc96b56c2b33bfa6627d4ba5ce
vincesaykao <vtsaykao@hotmail.com>
2022-03-18 13:42:01 -0500

    Added css and background login page, updated styles

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 
+import { Helmet } from 'react-helmet';
+
 function LoginPage() {
   const history = useHistory();
 
   return (
     <div>
       <LoginForm />
+      <Helmet>
+                <style>{`body,html {background-image: url("https://media.istockphoto.com/photos/portrait-of-senior-man-sitting-in-chair-picture-id1313927273?b=1&k=20&m=1313927273&s=170667a&w=0&h=YUBdN5oytR68Uv_5ckLNU6hMoPHwfhUfjjdmiYVpPII="); 
+                background-size: cover; background-position: -270px 0px;
+                }`}
+
+                </style>
+            </Helmet>
 
       {/* <center>
         <button
