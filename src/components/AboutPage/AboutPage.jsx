0c96b18874b343828291451c582ceb7d69893b0f
vincesaykao <vtsaykao@hotmail.com>
2022-03-21 14:11:20 -0500

    got about page to route back to login

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import img1 from './ILC.png'
+import { useHistory } from 'react-router-dom';
 
 import './AboutPage.scss';
 
@@ -8,12 +9,24 @@ import './AboutPage.scss';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is'
 
+
+
 function AboutPage() {
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/user')
+  }
+
+
+
   return (
     <div className="container">
       <div>
         <img 
         className='logo'
+        onClick={handleClick}
         src={img1}/>
       </div>
     </div>
