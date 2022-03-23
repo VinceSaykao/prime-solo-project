1a2167e8847c206cd454708f9838267a641c380e
vincesaykao <vtsaykao@hotmail.com>
2022-03-23 09:17:42 -0500

    changed clients names DOM on timesheet page

--- src/components/Timesheet/Timesheet.js
+++ src/components/Timesheet/Timesheet.js
@@ -161,7 +161,8 @@ Row.propTypes = {
 
 const rows = [
     createData('Bilbo Baggins'),
-    createData('Bilbo Baggins'),
+    createData('Michelle Sweden'),
+    createData('Naruto Suzuki'),
     // createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
     // createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
     // createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
@@ -236,10 +237,6 @@ export default function Timesheet() {
         },
 
     ];
-
-
-
-
 
     console.log('this is new', timesheetReducer.client_name);
     console.log('timesheet is', timesheetReducer);
