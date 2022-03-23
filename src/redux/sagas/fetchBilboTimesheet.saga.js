af75b89f0bcd631c01238c64641febb12a67b8ee
vincesaykao <vtsaykao@hotmail.com>
2022-03-23 15:23:17 -0500

    added fetchbilbotimesheet saga

---
+++ src/redux/sagas/fetchBilboTimesheet.saga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { put, takeEvery } from 'redux-saga/effects';
+
+
+// generator saga function: will be fired on "FETCH_TIMESHEET" actions
+function* fetchBilboTimesheet() {
+    try {
+        const response = yield axios.get('/api/timesheet/bilbo');
+        yield put({type: 'SET_BILBO_TIMESHEET', payload: response.data}); 
+    } catch(error) {
+        console.log('error fetching timesheet', error);
+    }
+
+
+
+
+}; // end of fetchTimesheet
+
+// watcher root saga
+export default function* rootSaga() {
+  yield takeEvery('FETCH_BILBO_TIMESHEET', fetchBilboTimesheet);
+}; // end of rootSaga
