6f3e99b97f9c4a8f6dd6d94b90b218f3ba652af7
vincesaykao <vtsaykao@hotmail.com>
2022-03-23 09:04:08 -0500

    commented out code and cleaned up mui

--- src/components/Timesheet/Timesheet.js
+++ src/components/Timesheet/Timesheet.js
@@ -160,11 +160,12 @@ Row.propTypes = {
 };
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+    createData('Bilbo Baggins'),
+    createData('Bilbo Baggins'),
+    // createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+    // createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+    // createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+    // createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
 ];
 
 
@@ -249,11 +250,11 @@ export default function Timesheet() {
                 <TableHead>
                     <TableRow>
                         <TableCell />
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
+                        <TableCell>Client Name</TableCell>
+                        {/* <TableCell align="right">Calories</TableCell>
                         <TableCell align="right">Fat&nbsp;(g)</TableCell>
                         <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
                     </TableRow>
                 </TableHead>
                 <TableBody>
