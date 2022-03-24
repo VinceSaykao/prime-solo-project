f945f3ed222a16827b2cce26f712cbb2cc7f026b
vincesaykao <vtsaykao@hotmail.com>
2022-03-23 19:49:31 -0500

    Added michelle and naruto get

--- server/routes/timesheet.router.js
+++ server/routes/timesheet.router.js
@@ -41,6 +41,40 @@ router.get('/bilbo', (req, res) => {
     };
 }); // end of GET
 
+// this will GET the Michelle Sweden timesheet from the database
+router.get('/michelle', (req, res) => {
+    // GET route code here
+
+    if (req.isAuthenticated()) {
+        pool
+            .query(`select TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = 2 order by date desc;`)
+            .then((results) => res.send(results.rows))
+            .catch((error) => {
+                console.log('Error making SELECT for get timesheet:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403); // Forbidden
+    };
+}); // end of GET
+
+// this will GET the Naruto Suzuki timesheet from the database
+router.get('/naruto', (req, res) => {
+    // GET route code here
+
+    if (req.isAuthenticated()) {
+        pool
+            .query(`select TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = 3 order by date desc;`)
+            .then((results) => res.send(results.rows))
+            .catch((error) => {
+                console.log('Error making SELECT for get timesheet:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403); // Forbidden
+    };
+}); // end of GET
+
 
 // this will POST the timesheet Form inputs to the database
 router.post('/', (req, res) => {
