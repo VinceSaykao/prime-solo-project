6053c2260b5dfd9bac5cf6e9352f4788f242992c
vincesaykao <vtsaykao@hotmail.com>
2022-03-24 21:08:26 -0500

    changed bilbo sql text

--- server/routes/timesheet.router.js
+++ server/routes/timesheet.router.js
@@ -25,12 +25,12 @@ router.get('/', (req, res) => {
 
 
 // this will GET the Bilbo Baggins timesheet from the database
-router.get('/bilbo', (req, res) => {
+router.get('/bilbo:id', (req, res) => {
     // GET route code here
 
     if (req.isAuthenticated()) {
         pool
-            .query(`select TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = 1 order by date desc;`)
+            .query(`select client_id,TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = $1 order by date asc;`)
             .then((results) => res.send(results.rows))
             .catch((error) => {
                 console.log('Error making SELECT for get timesheet:', error);
@@ -41,39 +41,6 @@ router.get('/bilbo', (req, res) => {
     };
 }); // end of GET
 
-// this will GET the Michelle Sweden timesheet from the database
-router.get('/michelle', (req, res) => {
-    // GET route code here
-
-    if (req.isAuthenticated()) {
-        pool
-            .query(`select TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = 2 order by date desc;`)
-            .then((results) => res.send(results.rows))
-            .catch((error) => {
-                console.log('Error making SELECT for get timesheet:', error);
-                res.sendStatus(500);
-            });
-    } else {
-        res.sendStatus(403); // Forbidden
-    };
-}); // end of GET
-
-// this will GET the Naruto Suzuki timesheet from the database
-router.get('/naruto', (req, res) => {
-    // GET route code here
-
-    if (req.isAuthenticated()) {
-        pool
-            .query(`select TO_CHAR("date",'MM-DD-YYYY'),client_name,"in","out",mileage,notes from timesheet join clients on clients.id = timesheet.client_id where clients.id = 3 order by date desc;`)
-            .then((results) => res.send(results.rows))
-            .catch((error) => {
-                console.log('Error making SELECT for get timesheet:', error);
-                res.sendStatus(500);
-            });
-    } else {
-        res.sendStatus(403); // Forbidden
-    };
-}); // end of GET
 
 
 // this will POST the timesheet Form inputs to the database
